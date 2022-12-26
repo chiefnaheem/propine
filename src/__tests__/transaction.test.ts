d2d103d5378ca971877d087bde76562b2f7e8e40
naheem adedokun <nadedokun@heckerbella.com>
2022-12-26 02:57:40 +0100

    get exchange range test

--- src/__tests__/transaction.test.ts
+++ src/__tests__/transaction.test.ts
@@ -1,2 +1,9 @@
-import * as utils from './utils';
+import * as utils from '../utils/helper';
 import { getExchangeRates, getTransactionsFromCSV, getPortfolioValue } from './';
+
+describe('getExchangeRates', () => {
+  it('should return the exchange rates for the given tokens', async () => {
+    const exchangeRates = await getExchangeRates(['BTC', 'ETH']);
+    expect(exchangeRates).toEqual({ BTC: 123.45, ETH: 67.89 });
+  });
+});
